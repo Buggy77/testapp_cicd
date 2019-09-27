f6f7e5059499e576f455f66369f028d0ab7ea95d
Bug <Bug@TSI-017>
2019-09-27 13:51:12 -0500

    Se agrega método para validar regla de Sonar

--- TestApp/src/main/java/com/test/TestApp/TestAppApplication.java
+++ TestApp/src/main/java/com/test/TestApp/TestAppApplication.java
@@ -1,5 +1,7 @@
 package com.test.TestApp;
 
+import javax.swing.text.StyledEditorKit.ForegroundAction;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -10,6 +12,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class TestAppApplication {
 
 	public static void main(String[] args) {
+		Integer valor = new TestAppApplication().metodoTest();
+		System.out.print(valor);
 		SpringApplication.run(TestAppApplication.class, args);
 	}
 	
@@ -22,6 +26,18 @@ public class TestAppApplication {
 		          "forward:/index.html");
 		    }
 		  };
+	}
+	
+	public Integer metodoTest() {
+		
+		Integer valor = 0;
+		
+		if (valor.compareTo(0) == 0) {
+			valor = 1 + 1;
+		} else if (valor.compareTo(1) > 0) {
+			valor = 1 +2;
+		} 
+		return valor;
 	}
 
 }
